e812648b6cd69687a8c1bc1b6630da16c19fffff
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-10-06 22:53:41 +0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "azurerm" {
-        resource_group_name  = "murapoc-remotestate-rg"
-        storage_account_name = "terraformremotestorage"
-        container_name       = "vmdevstate"
+        resource_group_name  = "TFstate-rg"
+        storage_account_name = "remotestateformstorage"
+        container_name       = "mura-tfstate"
         key                  = "testproject.tfstate"
     }
 }
