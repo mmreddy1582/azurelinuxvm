36b9463b6896484aadda7487b7539f8fe1412f15
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-10-30 15:49:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "linuxservers" {
   data_sa_type                  = "Premium_LRS"
   enable_ssh_key                = true
   vm_size                       = "Standard_D2s_v3"
-  vm_ipaddress                  = "10.1.0.6"
+  vm_ipaddress                  = "10.1.0.8"
   
   tags = {
     environment = "dev"
